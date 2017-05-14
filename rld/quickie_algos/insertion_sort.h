7671e175f2773e8473bdcace642a6163f7bfb97f
Rafal Rafalski <gotchas.u@gmail.com>
2017-05-14 23:36:03 +0200

    clang-format-5.0 reformatting

--- rld/quickie_algos/insertion_sort.h
+++ rld/quickie_algos/insertion_sort.h
@@ -2,18 +2,18 @@
 
 #include <algorithm>
 
-namespace rld { namespace algos {
+namespace rld {
+namespace algos {
 
 /// element which is going to be inserted is at the past the end position
 /// upper_bound returns destination for this element
 /// insertion is done by rotate left
-template <class It>
-void
-insertion_sort(It f, It e) {
-    auto const s = f;
-    for (; f != e; ++f) {
-        std::rotate(std::upper_bound(s, f, *f), f, f+1);
-    }
+template <class It> void insertion_sort(It f, It e) {
+  auto const s = f;
+  for (; f != e; ++f) {
+    std::rotate(std::upper_bound(s, f, *f), f, f + 1);
+  }
 }
 
-}}
+} // namespace algos
+} // namespace rld
--- rld/quickie_algos/selection_sort.h
+++ rld/quickie_algos/selection_sort.h
@@ -2,13 +2,14 @@
 
 #include <algorithm>
 
-namespace rld { namespace algos {
+namespace rld {
+namespace algos {
 
-template<class ForwardIt>
-void selection_sort(ForwardIt begin, ForwardIt end)
-{
-    for (ForwardIt i = begin; i != end; ++i)
-        std::iter_swap(i, std::min_element(i, end));
+template <class ForwardIt> void selection_sort(ForwardIt begin, ForwardIt end) {
+  for (ForwardIt i = begin; i != end; ++i) {
+    std::iter_swap(i, std::min_element(i, end));
+  }
 }
 
-}}
+} // namespace algos
+} // namespace rld
