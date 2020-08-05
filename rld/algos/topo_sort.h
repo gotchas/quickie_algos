920ca441ab094f3aa073181ce30cd760856a6a2b
rafall <rdzbek@gmail.com>
2020-08-05 17:59:20 +0200

    graph_out has dedicated header ; graph_out has now constructor from std::inittilizer

--- rld/algos/topo_sort.h
+++ rld/algos/topo_sort.h
@@ -1,55 +1,14 @@
 #pragma once
-#include <algorithm>
-#include <vector>
+
+#include "graph.h"
 
 namespace rld {
 namespace algos {
 
-   class Graph {  // TODO
-      std::vector<std::vector<int>> adj_;
-
-     public:
-      Graph(int edges) : adj_(edges) {}
-      // Graph(std::initializer_list<std::initializer_list<int>> adj): adj_{adj} {}
-      Graph(std::vector<std::vector<int>>&& adj) : adj_{std::move(adj)} {}
-
-      Graph&
-      add_edge(int s, int e) {
-         adj_[s].push_back(e);
-         return *this;
-      }
-      Graph&
-      remove_edge(int s, int e) {
-         auto& v = adj_.at(s);
-         auto  f = std::find(v.begin(), v.end(), e);
-         if (f != v.end()) {
-            *f = v.back();
-            v.pop_back();
-         }
-         return *this;
-      }
-      ///////
-      bool
-      connected(int s, int e) const {
-         auto const& v = adj_.at(s);
-         return std::find(v.begin(), v.end(), e) != v.end();
-      }
-      std::vector<int>
-      indegrees() const {
-         std::vector<int> ret(adj_.size());
-         namespace ranges = std::ranges;
-         ranges::for_each(std::as_const(adj_),
-                          [&ret](std::vector<int> const& v) { ranges::for_each(v, [&ret](int e) { ++ret[e]; }); });
-         return ret;
-      }
-      std::vector<int> const &
-      outcoming(int s) const { return adj_.at(s); }
-   };
-
    // Adjacency List, directed, acyclic
    // continuous ids for nodes starting from 0 - Int()
    std::vector<int>
-   topo_sort(Graph const& adj) {
+   topo_sort(graph_out const& adj) {
       namespace ranges = std::ranges;
       std::vector<int> indegrees{adj.indegrees()};
 
